a8593355a97c08894af209abc8de86d99551e8d6
houzhongqin <614977099@qq.com>
2020-09-01 11:58:26 +0800

    l

--- components/vue-demo4/src/router/index.js
+++ components/vue-demo4/src/router/index.js
@@ -5,12 +5,13 @@ Vue.use(VueRouter)
 // 引入路由组件
 import Home from "../pages/Home.vue"
 import About from "../pages/About.vue"
+
 export default new VueRouter({
-  mode:"hash", // 带#
-  routes:[
+  mode:"hash", // 模式 带#
+  routes:[  // 路由规则
     {
       path:"/home",  // 访问路径
-      component:Home
+      component:Home  // 路径对应显示的
     },
     {
       path:"/about",
